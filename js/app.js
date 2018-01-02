ca32371597a27649122839412cbbb82d16f09386
LeslyN <lnombertoc@gmail.com>
2018-01-02 07:59:10 -0500

    Empleando data

--- js/app.js
+++ js/app.js
@@ -4,74 +4,69 @@ $(document).ready(function() {
     $('#splash').fadeOut(300);
   }, 3000);
 
-  /* Mapa */ 
+  /* Iniciando Mapa */ 
   var options = {
-    zoom: 8
-    , center: new google.maps.LatLng(18.2, -66.4)
-    , mapTypeId: google.maps.MapTypeId.SATELLITE
+    zoom: 8, 
+    center: new google.maps.LatLng(18.2, -66.4), 
+    mapTypeId: google.maps.MapTypeId.SATELLITE, 
+    backgroundColor: '#ffffff', 
+    noClear: true, 
+    disableDefaultUI: true, 
+    keyboardShortcuts: false, 
+    disableDoubleClickZoom: true, 
+    draggable: false, 
+    scrollwheel: false, 
+    draggableCursor: 'move', 
+    draggingCursor: 'move', 
+    mapTypeControl: true,
 
-    , backgroundColor: '#ffffff'
- 		, noClear: true
-    , disableDefaultUI: true
-    , keyboardShortcuts: false
-    , disableDoubleClickZoom: true
-    , draggable: false
-    , scrollwheel: false
-    , draggableCursor: 'move'
-    , draggingCursor: 'move'
-
-    , mapTypeControl: true
-    , mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.HORIZONTAL_MENU
-      , position: google.maps.ControlPosition.TOP_LEFT
-      , mapTypeIds: [
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_MENU, 
+      position: google.maps.ControlPosition.TOP_LEFT, 
+      mapTypeIds: [
         google.maps.MapTypeId.SATELLITE
       ]
-    }
-    , navigationControl: true
-    , streetViewControl: true
-    , navigationControlOptions: {
-      position: google.maps.ControlPosition.TOP_RIGHT
-      , style: google.maps.NavigationControlStyle.ANDROID
-    }
-    , scaleControl: true
-    , scaleControlOptions: {
-      position: google.maps.ControlPosition.TOP_LEFT
-      , style: google.maps.ScaleControlStyle.DEFAULT
+    }, 
+    navigationControl: true, 
+    streetViewControl: true, 
+    navigationControlOptions: {
+      position: google.maps.ControlPosition.TOP_RIGHT, 
+      style: google.maps.NavigationControlStyle.ANDROID
+    }, 
+    scaleControl: true, 
+    scaleControlOptions: {
+      position: google.maps.ControlPosition.TOP_LEFT, 
+      style: google.maps.ScaleControlStyle.DEFAULT
     }
   };
 
   var map = new google.maps.Map(document.getElementById('map'), options);
 
   map.setOptions({
-    zoom: 10
-    , center: new google.maps.LatLng(18.17, -66.42)
-    , mapTypeId: google.maps.MapTypeId.TERRAIN
-
-    , keyboardShortcuts: true
-    , disableDoubleClickZoom: false
-    , draggable: true
-    , scrollwheel: true
-    , draggableCursor: 'hand'
-    , draggingCursor: 'hand'
-
-    , mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-      , position: google.maps.ControlPosition.TOP_RIGHT
-      , mapTypeIds: [
-        google.maps.MapTypeId.ROADMAP
-        , google.maps.MapTypeId.SATELLITE
+    zoom: 10, 
+    center: new google.maps.LatLng(18.17, -66.42), 
+    mapTypeId: google.maps.MapTypeId.TERRAIN, 
+    keyboardShortcuts: true, 
+    disableDoubleClickZoom: false, 
+    draggable: true, 
+    scrollwheel: true, 
+    draggableCursor: 'hand', 
+    draggingCursor: 'hand', 
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU, 
+      position: google.maps.ControlPosition.TOP_RIGHT, 
+      mapTypeIds: [
+        google.maps.MapTypeId.ROADMAP, 
+        google.maps.MapTypeId.SATELLITE
       ]
-    }
-
-    , navigationControlOptions: {
-      position: google.maps.ControlPosition.TOP_LEFT
-      , style: google.maps.NavigationControlStyle.ZOOM_PAN
-    }
-
-    , scaleControlOptions: {
-      position: google.maps.ControlPosition.BOTTOM_LEFT
-      , style: google.maps.ScaleControlStyle.DEFAULT
+    }, 
+    navigationControlOptions: {
+      position: google.maps.ControlPosition.TOP_LEFT, 
+      style: google.maps.NavigationControlStyle.ZOOM_PAN
+    }, 
+    scaleControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_LEFT,
+      style: google.maps.ScaleControlStyle.DEFAULT
     }
   });
 
@@ -84,7 +79,7 @@ $(document).ready(function() {
   map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
   var mapTypeIdOfMap = map.getMapTypeId();
 
-  alert(zoomLevel + ' -- ' + centerOfMap + ' -- ' + mapTypeIdOfMap);
+  // alert(zoomLevel + ' -- ' + centerOfMap + ' -- ' + mapTypeIdOfMap);
 
   $nav = $('.navbar-brand');
   $nav.css('color', '#B9763A');
---
+++ js/data.js
@@ -0,0 +1,15 @@
+var data = {
+  COMIN: {
+    name: 'Comida india',
+    adress: 'Av. Caminos del inca 2120',
+    price: 'S/ 30',
+    image: '../assest/images/comida-india.jpg'
+  },
+
+  POLLOPLA: {
+    name: 'Pollo a la plancha',
+    adress: 'Av. Tomás Marsano 278',
+    price: 'S/ 10',
+    image: '../assest/images/pollo-plancha.jpg'
+  }
+};
